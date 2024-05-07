072e01793c8978e95883e14a0db68ccb099bc26c
lior-danielov <liordani444555@gmail.com>
2024-05-07 18:31:32 +0300

    update example

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Navbar from "@/utils/components/navbar/navbar";
+import Marketplace from "@/utils/components/marketplace/marketplace";
+import Groups from "@/utils/components/groups/groups";
+import Login from "@/utils/components/login/login";
+
+
+export default function Home() {
+  return (
+      <div>
+
+       <Navbar />
+       <Marketplace />
+       <Groups />
+       <Login />
+
+      </div>
+  );
+}
